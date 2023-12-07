9ab262befea4cf2dfb6227f5b4780fe36e4e2ae8
lars-de-vries <112874252+lars-de-vries@users.noreply.github.com>
2023-12-07 13:49:16 +0100

    js update

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,57 @@ var jumpAnimation = document.querySelector('.jump-animation');
 var danceAnimation = document.querySelector('.dance-animation');
 
 
+// waveButton.addEventListener('click', function(){
+//     idleAnimation.classList.add('hide');
+//     idleAnimation.pause();
+//     idleAnimation.currentTime = 0;
+
+//     jumpAnimation.classList.add('hide');
+//     danceAnimation.classList.add('hide');
+//     waveAnimation.classList.remove('hide');
+//     waveAnimation.play();
+//     setTimeout( () => {
+//         waveAnimation.classList.add('hide');
+//         idleAnimation.classList.remove('hide');
+//         idleAnimation.play();
+//     }, 2100);
+// });
+
+// jumpButton.addEventListener('click', function(){
+//     idleAnimation.classList.add('hide');
+//     idleAnimation.pause();
+//     idleAnimation.currentTime = 0;
+
+//     waveAnimation.classList.add('hide');
+//     danceAnimation.classList.add('hide');
+//     jumpAnimation.classList.remove('hide');
+//     jumpAnimation.play();
+//     setTimeout( () => {
+//         jumpAnimation.classList.add('hide');
+//         idleAnimation.classList.remove('hide');
+//         idleAnimation.play();
+//     }, 2100);
+// });
+
+// danceButton.addEventListener('click', function(){
+//     idleAnimation.classList.add('hide');
+//     idleAnimation.pause();
+//     idleAnimation.currentTime = 0;
+
+//     waveAnimation.classList.add('hide');
+//     jumpAnimation.classList.add('hide');
+//     danceAnimation.classList.remove('hide');
+//     danceAnimation.play();
+//     setTimeout( () => {
+//         danceAnimation.classList.add('hide');
+//         idleAnimation.classList.remove('hide');
+//         idleAnimation.play();
+//     }, 2100);
+// });
+
+
+
+
 waveButton.addEventListener('click', function(){
     idleAnimation.classList.add('hide');
     idleAnimation.pause();
@@ -21,12 +72,14 @@ waveButton.addEventListener('click', function(){
     danceAnimation.classList.add('hide');
     waveAnimation.classList.remove('hide');
     waveAnimation.play();
-    setTimeout( () => {
+
+    waveAnimation.addEventListener("ended", (event) => {
         waveAnimation.classList.add('hide');
         idleAnimation.classList.remove('hide');
         idleAnimation.play();
-    }, 2100);
+    });
 });
+
 
 jumpButton.addEventListener('click', function(){
     idleAnimation.classList.add('hide');
@@ -37,12 +90,14 @@ jumpButton.addEventListener('click', function(){
     danceAnimation.classList.add('hide');
     jumpAnimation.classList.remove('hide');
     jumpAnimation.play();
-    setTimeout( () => {
+
+    jumpAnimation.addEventListener("ended", (event) => {
         jumpAnimation.classList.add('hide');
         idleAnimation.classList.remove('hide');
         idleAnimation.play();
-    }, 2100);
+    });
 });
+
 
 danceButton.addEventListener('click', function(){
     idleAnimation.classList.add('hide');
@@ -53,13 +108,10 @@ danceButton.addEventListener('click', function(){
     jumpAnimation.classList.add('hide');
     danceAnimation.classList.remove('hide');
     danceAnimation.play();
-    setTimeout( () => {
+
+    danceAnimation.addEventListener("ended", (event) => {
         danceAnimation.classList.add('hide');
         idleAnimation.classList.remove('hide');
         idleAnimation.play();
-    }, 2100);
+    });
 });
-
-
-
-
